c1169dabc19e82c59d4f58e8cc630540b349ffcd
Pand3ru <github@pankratov.de>
2026-04-03 11:55:00 +0200

    Fix permission issue

--- system/desk.nix
+++ system/desk.nix
@@ -1,7 +1,7 @@
 { config, pkgs, userSettings, ... }:
 {
   services.caddy.virtualHosts."http://desk.home".extraConfig = ''
-    root * ${userSettings.installationPath}/assets/desk/
+    root * /srv/desk/
     file_server
   '';
 }
