acc3fe57ae4460fd32affb30bc5ae11d82ee9c32
Pand3ru <github@pankratov.de>
2026-04-03 11:50:31 +0200

    Restructure assets

--- system/desk.nix
+++ system/desk.nix
@@ -1,7 +1,7 @@
 { config, pkgs, userSettings, ... }:
 {
   services.caddy.virtualHosts."http://desk.home".extraConfig = ''
-    root * ${userSettings.installationPath}/assets/desk.html
+    root * ${userSettings.installationPath}/assets/desk/
     file_server
   '';
 }
