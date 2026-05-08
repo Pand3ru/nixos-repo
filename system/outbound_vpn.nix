195df0e167a5285811eeb2de653a90948fae2c9c
Pand3ru <github@pankratov.de>
2026-05-08 18:04:35 +0200

    Update vpn settings

--- system/outbound_vpn.nix
+++ system/outbound_vpn.nix
@@ -7,13 +7,15 @@ in
   networking.wireguard.interfaces.outbound_vpn = {
     privateKeyFile = sk;
     interfaceNamespace = "vpn";
-    ips = [ "10.2.0.2/32" "2a07:b944::2:2/128" ];
+    #ips = [ "10.2.0.2/32" "2a07:b944::2:2/128" ];
+    ips = [ "10.2.0.2/32" ];
     # This option does not seem to exist. See: https://www.reddit.com/r/NixOS/comments/1dyjrbb/setting_dns_server_for_wireguard_interface/
     #dns = [ "10.2.0.1" "2a07:b944::2:1" ]; 
     peers = [{
       publicKey = "m0HsRUbWzTEDSNH9BVrsnGaA9mGaHfQlLDD2ngzgJmc=";
       endpoint = "37.120.137.226:51820";
       allowedIPs = [ "0.0.0.0/0" ];
+      #allowedIPs = [ "0.0.0.0/0" "::/0" ];
       persistentKeepalive = 25;
     }];
   };
